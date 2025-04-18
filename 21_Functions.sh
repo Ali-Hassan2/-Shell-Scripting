53aa45cc39c067970486b7e64f673d90e2df3540
Ali-Hassan2 <alihassan26032004@gmail.com>
2025-04-18 22:59:52 +0500

    Functions Script added

---
+++ 21_Functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# method 1 of creating a method or function
+function greetings(){
+	echo "Aslam alaikum"
+}
+
+greetings
+
+# with values
+
+ageprinting(){
+	echo "Welcome  $1"
+	echo "Your age is: $2"
+}
+
+# calling the functions
+ageprinting Ali 22
+ageprinting Shoaib 12
+
+
+
+
+
